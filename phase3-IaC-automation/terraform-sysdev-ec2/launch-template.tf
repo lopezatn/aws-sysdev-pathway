19535073b4e2df8663836aea45f781cd19aa6cd6
lopezatn <agustinlpzb@gmail.com>
2025-12-08 14:53:56 +0100

    Modification to image_id data source

--- phase3-IaC-automation/terraform-sysdev-ec2/launch-template.tf
+++ phase3-IaC-automation/terraform-sysdev-ec2/launch-template.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "sysdev_web_lt" {
   name_prefix   = "sysdev-web-"
-  image_id      = "/aws/service/canonical/ubuntu/server/jammy/stable/current/amd64/hvm/ebs-gp2/ami-id"
+  image_id      = "data.aws_ssm_parameter.ubuntu_ami.value"
   instance_type = "t3.micro"
 
   iam_instance_profile {
@@ -11,5 +11,5 @@ resource "aws_launch_template" "sysdev_web_lt" {
     aws_security_group.web_sg.id,
   ]
 
-  user_data = base64encode(file("~/Documents/aws-sysdev-pathway/phase3-IaC-automation/terraform-sysdev-ec2/userdata-script.sh"))
+  user_data = base64encode(file("/Documents/aws-sysdev-pathway/phase3-IaC-automation/terraform-sysdev-ec2/userdata-script.sh"))
 }
